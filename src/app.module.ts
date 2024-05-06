d3cd5fa1551492a832af0c8d7d95a50eec878cf8
ANDERSON <andersoncassio2008@gmail.com>
2024-05-06 13:54:03 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import {} from './app.controller';
+import {} from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
