4930e9173698e3dd0d01f55dd289d3621a6fc49d
ANDERSON <andersoncassio2008@gmail.com>
2024-05-09 18:08:38 -0300

    feat: implementação de requisitos na entidade vacancy

--- src/auth/Config/dtos/Vacancy/vacancy-dto.ts
+++ src/auth/Config/dtos/Vacancy/vacancy-dto.ts
@@ -9,27 +9,34 @@ import {
 import { UserRoleEnum } from '../../enums/userRoleEnum';
 
 export class CreateVacancyDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'Job vacancy is mandatory' })
+  @IsNotEmpty({ message: 'Vacancy role is required ' })
   vacancyRole: string;
 
   @IsNumber()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'wage is required' })
   wage: number;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'location must be a string' })
+  @IsNotEmpty({ message: 'Location is required' })
   location: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'Vacancy type must be a string' })
+  @IsNotEmpty({ message: 'Vacancy type is mandatory' })
   vacancyType: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'description must be a string' })
+  @IsNotEmpty({ message: 'Description is required' })
   vacancyDescription: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'level must be a string' })
+  @IsNotEmpty({ message: 'level is required' })
   level: string;
+
+  @IsString({ message: 'The company name must be a string' })
+  @IsNotEmpty({
+    message:
+      "The name of the company is necessary to create the vacancy, if you can't find your company it must be created first!",
+  })
+  companyId: string;
 }
--- src/vacancy/vacancy.controller.ts
+++ src/vacancy/vacancy.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { VacancyService } from './vacancy.service';
 import { CreateVacancyDto, UpdateVacancyDto } from 'src/auth/Config';
 
