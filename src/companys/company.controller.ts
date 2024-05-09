3468c84e24ac98a1f7a721085946b6f32ad3248d
Hamilton Gonçalves <hamiltongsj@gmail.com>
2024-05-08 23:04:20 -0300

    feat: inserindo o controller do company, com as rotas de create e update

---
+++ src/companys/company.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Patch,
+  Param,
+  ParseIntPipe,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
+import { CompanyService } from './company.service';
+import { CreateCompanyDto } from '../Docs/companys/create-company.dto';
+import { AuthGuard, RoleGuard, UserRoleEnum } from 'src/auth/Config';
+import { Roles } from 'src/auth/Config/decorators/roles.decorator';
+import { UpdateCompanyDto } from 'src/Docs';
+
+@ApiTags('Company')
+@UseGuards()
+@Controller('company')
+export class CompanyController {
+  constructor(private readonly companyService: CompanyService) {}
+
+  @UseGuards(AuthGuard, RoleGuard)
+  @Roles(UserRoleEnum.admin)
+  @HttpCode(HttpStatus.ACCEPTED)
+  @Post()
+  async create(@Body() createCompanyDto: CreateCompanyDto) {
+    return await this.companyService.create(createCompanyDto);
+  }
+
+  @UseGuards(AuthGuard, RoleGuard)
+  @Roles(UserRoleEnum.admin)
+  @HttpCode(HttpStatus.ACCEPTED)
+  @Patch(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateCompanyDto,
+  ) {
+    return await this.companyService.update(id, payload);
+  }
+}
