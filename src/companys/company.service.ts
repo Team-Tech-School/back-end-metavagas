eebb98ff0e5ce66cc6cf9a0b361e7e341108fd3c
Hamilton Gonçalves <hamiltongsj@gmail.com>
2024-05-08 23:02:27 -0300

    feat: inseridno o service do company, com as funções de create, update e idPicker

---
+++ src/companys/company.service.ts
@@ -0,0 +1,56 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { CreateCompanyDto } from '../Docs/companys/create-company.dto';
+import { Company } from 'src/Database/entities';
+import { UpdateCompanyDto } from 'src/Docs';
+
+@Injectable()
+export class CompanyService {
+  constructor(
+    @InjectRepository(Company)
+    private companyRepository: Repository<Company>,
+  ) {}
+
+  async create(payload: CreateCompanyDto) {
+    try {
+      const newComany = this.companyRepository.create(payload);
+
+      await this.companyRepository.save(newComany);
+
+      return newComany;
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async update(id: number, payload: UpdateCompanyDto) {
+    await this.idPicker(id);
+
+    await this.companyRepository.update(id, payload);
+
+    return await this.idPicker(id);
+  }
+
+  async idPicker(id: number) {
+    try {
+      const company = await this.companyRepository.findOne({ where: { id } });
+
+      if (!company) {
+        throw new NotFoundException(`An user with this id:${id} not found`);
+      }
+
+      return company;
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+}
