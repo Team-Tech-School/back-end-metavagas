3192750e797ce1cc9c832d1455a5ab426ef97609
Hamilton Gonçalves <hamiltongsj@gmail.com>
2024-05-09 21:48:44 -0300

    feat: ajustando a importação dos dtos

--- src/auth/Config/dtos/index.ts
+++ src/auth/Config/dtos/index.ts
@@ -2,3 +2,7 @@ export * from './Auth/Login-dto';
 export * from './Auth/create-user.dto';
 export * from './Auth/update-user.dto';
 export * from './Vacancy/vacancy-dto';
+export * from './companys/create-company.dto';
+export * from './companys/update-company.dto';
+export * from './technologys/create-technology.dto';
+export * from './technologys/update-technology.dto';
