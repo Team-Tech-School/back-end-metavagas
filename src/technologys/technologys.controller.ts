8d10c55a85d389c68559ebe653f7d4b71c834397
Hamilton Gonçalves <hamiltongsj@gmail.com>
2024-05-09 22:11:20 -0300

    feat: implementando a rota de criação de uma technology

--- src/technologys/technologys.controller.ts
+++ src/technologys/technologys.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 
 import { TechnologysService } from './technologys.service';
 import { CreateTechnologyDto } from '../auth/Config/dtos';
+import { Roles } from 'src/auth/Config/decorators/roles.decorator';
+import { AuthGuard, RoleGuard, UserRoleEnum } from 'src/auth/Config';
 
 @Controller('technologys')
 export class TechnologysController {
   constructor(private readonly technologysService: TechnologysService) {}
 
+  @UseGuards(AuthGuard, RoleGuard)
+  @Roles(UserRoleEnum.admin)
+  @HttpCode(HttpStatus.ACCEPTED)
   @Post()
-  create(@Body() createTechnologyDto: CreateTechnologyDto) {
-    return this.technologysService.create(createTechnologyDto);
+  async create(@Body() payload: CreateTechnologyDto) {
+    return await this.technologysService.create(payload);
   }
 }
