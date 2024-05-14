735e4bf038c6fac1d510517cc530113f94aeb486
Anderson Rodrigues <127049907+dev-anderson-rodrigues@users.noreply.github.com>
2024-05-14 17:10:52 -0300

    Update database.config.ts

--- src/Database/database.config.ts
+++ src/Database/database.config.ts
@@ -16,11 +16,11 @@ export default <TypeOrmModuleAsyncOptions>{
   ): Promise<TypeOrmModuleOptions> => {
     return <PostgresConnectionOptions>{
       type: 'postgres',
-      host: configService.get('DB_HOST' || 'PGHOST'),
-      port: +configService.get('DB_PORT' || 'PGPORT'),
-      username: configService.get('DB_USERNAME' || 'POSTGRES_USER'),
-      password: configService.get('DB_PASSWORD' || 'POSTGRES_PASSWORD'),
-      database: configService.get('DB_NAME' || 'PGDATABASE'),
+      host: configService.get('PGHOST'),
+      port: +configService.get('PGPORT'),
+      username: configService.get('POSTGRES_USER'),
+      password: configService.get('POSTGRES_PASSWORD'),
+      database: configService.get('PGDATABASE'),
       entities: [User, Vacancy, Technology, Company],
       synchronize: true,
       logging: true,
