dccc4a59a1f45effa5ee9d4c244c9b16d4489f0f
Hamilton Gonçalves <hamiltongsj@gmail.com>
2024-05-14 20:33:37 -0300

    refactor: ajuste de importações e padronização de código - src

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { DatabaseModule } from './Database';
 import { UsersModule } from './users';
 import { AuthModule } from './auth/auth.module';
 import { CompanyModule } from './companys/company.module';
 import { VacancyModule } from './vacancy/vacancy.module';
 import { TechnologysModule } from './technologys/technologys.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
