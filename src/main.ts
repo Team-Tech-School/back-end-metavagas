42777b49750efa3aaef24f209476e3685cd00fc9
Anderson Rodrigues <127049907+dev-anderson-rodrigues@users.noreply.github.com>
2024-05-21 10:56:28 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   app.setGlobalPrefix('v1/');
   app.use(cors());
   app.enableCors({
-    origin: 'https://meta-vagas.netlify.app/' || 'https://localhost:5173',
+    origin: 'https://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
