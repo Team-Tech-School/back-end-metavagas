9928319306924205e55aedf5508116d9d9265fd3
ANDERSON <andersoncassio2008@gmail.com>
2024-05-30 22:20:03 -0300

    refactor: correção mock test vacancy

--- testing/vacancy/vacancy.mock.ts
+++ testing/vacancy/vacancy.mock.ts
@@ -11,7 +11,7 @@ export const VacancyMock = {
   technologies: [
     {
       id: 1,
-      tecName: 'typescript',
+      tecName: 'Typescript',
       creatorsName: 'Creator 1',
       createAt: new Date('2024-05-25T05:47:00.812Z'),
       updateAt: new Date('2024-05-25T05:47:00.812Z'),
