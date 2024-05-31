4b3b77f1a2dffc3ecb017dccfdb529ac55cc4ff4
ANDERSON <andersoncassio2008@gmail.com>
2024-05-31 16:26:32 -0300

    Ajuste cors

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
 
   app.use(cors());
   app.enableCors({
-    origin: 'CORS_LOCAL',
+    origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
