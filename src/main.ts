fa6f61a47420004287e1f006a9c9f403b8b2e014
ANDERSON <andersoncassio2008@gmail.com>
2024-05-30 23:50:20 -0300

    mudança credencials chamada api para o front

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
 
   app.use(cors());
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://meta-vagas.netlify.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
