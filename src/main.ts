f886c6d38bfd3f2198eca331ca7202a3a1058b1a
ANDERSON <andersoncassio2008@gmail.com>
2024-05-31 15:34:00 -0300

    mudanã cors da aplicação

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
 
   app.use(cors());
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'CORS_LOCAL',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
